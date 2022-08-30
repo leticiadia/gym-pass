379787a6af4042202f238b940f295b087313783c
Leticia Dias <contatoleticiadia@gmail.com>
2022-08-29 22:06:58 -0300

    feat: route to scheduling created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SchedulingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/agendar-aula', [SchedulingController::class, 'index']);
+Route::post('/agendar-aula', [SchedulingController::class, 'store']);
